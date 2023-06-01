f721cc3faf224a8202e4d3d61f149123d418baef
lzmgl <lzmgl5874@gmail.com>
2023-06-01 18:38:58 +0900

    text input query

--- pages/3-SQL.py
+++ pages/3-SQL.py
@@ -2,17 +2,7 @@ import streamlit as st
 
 from PIL import Image # 파이썬 기본라이브러리는 바로 사용 가능!
 import os
-path_now = os.path.dirname(os.path.abspath(__file__))
-dog_image_paths=os.path.join(path_now, '..', 'esset', 'dog')
-dog_image_files=os.listdir(dog_image_paths)
 
-# select = st.sidebar.selectbox('사진 3개중에 골라', dog_image_files)
-with st.sidebar:
-    add_radio = st.radio(
-        "개 사진 골라",
-        (dog_image_files)
-    )
-st.image(Image.open(os.path.join(dog_image_paths, add_radio)))
 
 
 import pymysql
@@ -33,15 +23,27 @@ db = pymysql.connect(
     db=database_name,   # Database name
     charset='utf8'
 )
+
+
+
+st.title("INDEX") 
+
 import time
 start = time.time()
-SQL = '''SELECT * FROM salaries WHERE emp_no=20000;'''
+SQL = st.text_input('Enter some text')
+# SQL = '''SELECT * FROM salaries WHERE emp_no=20000;'''
 cursor = db.cursor()
 cursor.execute(SQL)
 result = cursor.fetchall()
 df=pd.DataFrame(result)
 st.write(
-    "sql = ", SQL,"\n",
+    "sql = ", SQL, "\n",
     "result = ", df,
     "time = ", time.time()-start
 )
+
+with st.sidebar:
+    add_radio = st.radio(
+        "개 사진 골라",
+        (dog_image_files)
+    )
