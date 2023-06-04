a797a54602b2d95a9650352e3a4562c6eaaef139
lzmgl <lzmgl5874@gmail.com>
2023-06-04 16:22:40 +0900

    20230604_58

--- main_page.py
+++ main_page.py
@@ -9,42 +9,34 @@ import pymysql
 import pandas as pd
 
 
-host_name = "woori-fisa.cfnz7hfzq9bn.ap-northeast-2.rds.amazonaws.com"
-host_port = 3306
-username = "admin"
-password = "woorifisa1!"
-database_name = 'SQL_IMPROVE'
-db = pymysql.connect(
-    host=host_name,     # MySQL Server Address
-    port=host_port,          # MySQL Server Port
-    user=username,      # MySQL username
-    passwd=password,    # password for MySQL username
-    db=database_name,   # Database name
-    charset='utf8'
-)
-
-
-
 st.title("ERD") 
 _abspath = os.path.dirname(os.path.abspath(__file__))
-image_path1 = _abspath + '/1.png'
 
-image1 = Image.open(image_path1)
-st.image(image1)
+image0 = Image.open(_abspath + '/1.png')
+st.image(image0)
+
+
 
 st.header("Index 활용 실습") 
-
+image1 = Image.open(_abspath + '/1.png')
+st.image(image1)
 st.write(
     "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2년이상 거래가 없는 계좌"
 )
 
 st.header("partitioning 활용 실습") 
 
+st.header("Index 활용 실습") 
+image2 = Image.open(_abspath + '/1.png')
+st.image(image2)
 st.write(
     "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2년이상 거래가 없는 계좌"
 )
 st.header("window function 활용 실습") 
 
+st.header("Index 활용 실습") 
+image3 = Image.open(_abspath + '/1.png')
+st.image(image3)
 st.write(
     "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2년이상 거래가 없는 계좌"
 )
