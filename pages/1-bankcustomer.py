fea6efe1b24a16aa918905170355e55a5666b0ce
lzmgl <lzmgl5874@gmail.com>
2023-06-04 12:49:04 +0900

    20230604_3

--- pages/1-bankcustomer.py
+++ pages/1-bankcustomer.py
@@ -63,3 +63,19 @@ cursor.execute(SQL)
 columns = cursor.description 
 result = [{columns[index][0]:column for index, column in enumerate(value)} for value in cursor.fetchall()]
 df=pd.DataFrame(result)
+st.write(
+    btnResult = st.form_submit_button('Run')
+)
+
+if btnResult:
+    st.sidebar.text(f'Button pushed')
+    start = time.time()
+    SQL='''SELECT id,balance,last_date FROM bankcustomertest WHERE balance>=10 AND balance<50;'''
+    cursor.execute(SQL)
+    columns = cursor.description 
+    result = [{columns[index][0]:column for index, column in enumerate(value)} for value in cursor.fetchall()]
+
+df=pd.DataFrame(result)
+st.write(
+    df
+)
