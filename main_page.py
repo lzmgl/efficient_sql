2ddd3a540d1b6066209abd876820c778ccc000b2
lzmgl <lzmgl5874@gmail.com>
2023-06-04 16:24:22 +0900

    20230604_60

--- main_page.py
+++ main_page.py
@@ -12,13 +12,13 @@ import pandas as pd
 st.title("ERD") 
 _abspath = os.path.dirname(os.path.abspath(__file__))
 
-image0 = Image.open(_abspath + '/1.png')
+image0 = Image.open(_abspath + '/esset/1.png')
 st.image(image0)
 
 
 
 st.header("Index 활용 실습") 
-image1 = Image.open(_abspath + '/1.png')
+image1 = Image.open(_abspath + '/esset/1.png')
 st.image(image1)
 st.write(
     "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2년이상 거래가 없는 계좌"
@@ -26,14 +26,14 @@ st.write(
 
 st.header("partitioning 활용 실습") 
 
-image2 = Image.open(_abspath + '/1.png')
+image2 = Image.open(_abspath + '/esset/1.png')
 st.image(image2)
 st.write(
     "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2년이상 거래가 없는 계좌"
 )
 st.header("window function 활용 실습") 
 
-image3 = Image.open(_abspath + '/1.png')
+image3 = Image.open(_abspath + '/esset/1.png')
 st.image(image3)
 st.write(
     "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2년이상 거래가 없는 계좌"
