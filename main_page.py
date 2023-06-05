8bbabfc94f52288cb48c5f8d11e201432b1c8884
lzmgl <lzmgl5874@gmail.com>
2023-06-05 10:43:08 +0900

    sql add

--- main_page.py
+++ main_page.py
@@ -18,23 +18,30 @@ st.image(image0)
 
 
 st.header("Index 활용 실습") 
-image1 = Image.open(_abspath + '/esset/1.png')
-st.image(image1)
 st.write(
     "ex) id가 20000인 정보"
 )
 
+image1 = Image.open(_abspath + '/esset/1.png')
+st.image(image1)
+st.write(
+    "sql"
+)
 st.header("partitioning 활용 실습") 
-
-image2 = Image.open(_abspath + '/esset/1.png')
-st.image(image2)
 st.write(
     "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2018년 1월 1일 이후 거래가 없는 계좌"
 )
+image2 = Image.open(_abspath + '/esset/1.png')
+st.image(image2)
+st.write(
+    "sql"
+)
 st.header("window function 활용 실습") 
-
+st.write(
+    "ex) 지난 해와 연봉 차이"
+)
 image3 = Image.open(_abspath + '/esset/1.png')
 st.image(image3)
 st.write(
-    "ex) 지난 해와 연봉 차이"
+    "sql"
 )
