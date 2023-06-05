ec562cd4793503613e308e574ff7e884e933e67a
lzmgl <lzmgl5874@gmail.com>
2023-06-05 10:38:13 +0900

    modify ex

--- main_page.py
+++ main_page.py
@@ -21,7 +21,7 @@ st.header("Index 활용 실습")
 image1 = Image.open(_abspath + '/esset/1.png')
 st.image(image1)
 st.write(
-    "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2년이상 거래가 없는 계좌"
+    "ex) id가 20000인 정보"
 )
 
 st.header("partitioning 활용 실습") 
@@ -29,7 +29,7 @@ st.header("partitioning 활용 실습")
 image2 = Image.open(_abspath + '/esset/1.png')
 st.image(image2)
 st.write(
-    "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2년이상 거래가 없는 계좌"
+    "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2018년 1월 1일 이후 거래가 없는 계좌"
 )
 st.header("window function 활용 실습") 
 
