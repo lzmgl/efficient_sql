9b76d8aed0ae314012a14e088135d919dc36d6d4
lzmgl <lzmgl5874@gmail.com>
2023-06-05 11:03:07 +0900

    ex

--- main_page.py
+++ main_page.py
@@ -25,7 +25,16 @@ st.write(
 image1 = Image.open(_abspath + '/esset/1.png')
 st.image(image1)
 st.write(
+    "Index 활용 X"
+)
+st.write(
     "SELECT * FROM sal_ran WHERE emp_no=20000;"
+)
+st.write(
+    "Index 활용 O"
+)
+st.write(
+    "SELECT * FROM salaries WHERE emp_no=20000;"
 )
 st.header("partitioning 활용 실습 : 거래중지대상 계좌") 
 st.write(
@@ -34,7 +43,16 @@ st.write(
 image2 = Image.open(_abspath + '/esset/1.png')
 st.image(image2)
 st.write(
+    "Partition 활용 X"
+)
+st.write(
     "SELECT id,balance,last_date FROM bct_amt WHERE balance>=10 AND balance<50;"
+)
+st.write(
+    "Partition 활용 O"
+)
+st.write(
+    "SELECT id,balance,last_date FROM bct_amt_part WHERE balance>=10 AND balance<50;"
 )
 st.header("window function 활용 실습 : 전 행과의 차이값 계산") 
 st.write(
