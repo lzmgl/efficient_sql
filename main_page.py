8959bda4d3c18851f1e64c94ad9ede2c283dad7e
lzmgl <lzmgl5874@gmail.com>
2023-06-05 17:52:55 +0900

    image

--- main_page.py
+++ main_page.py
@@ -22,7 +22,7 @@ st.write(
     "ex) id가 20000인 정보"
 )
 
-image1 = Image.open(_abspath + '/esset/1.png')
+image1 = Image.open(_abspath + '/esset/index.png')
 # st.image(image1)
 st.write(
     "Index 활용 X"
@@ -40,7 +40,7 @@ st.header("partitioning 활용 실습 : 거래중지대상 계좌")
 st.write(
     "ex) 예금 잔액이 1만원이상~5만원 미만이면서 2018년 1월 1일 이후 거래가 없는 계좌"
 )
-image2 = Image.open(_abspath + '/esset/1.png')
+image2 = Image.open(_abspath + '/esset/partition.png')
 st.image(image2)
 st.write(
     "Partition 활용 X"
@@ -58,7 +58,7 @@ st.header("window function 활용 실습 : 행의 순번 반환")
 st.write(
     "ex) 연 평균 잔고 순위"
 )
-image3 = Image.open(_abspath + '/esset/1.png')
+image3 = Image.open(_abspath + '/esset/window.png')
 # st.image(image3)
 st.write(
     'window 활용 O'
